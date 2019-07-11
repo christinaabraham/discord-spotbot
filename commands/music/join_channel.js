bc5d01d50dea44b3c466abc6905818fab85967d0
Christina Abraham <40750751+christinaabraham@users.noreply.github.com>
2019-07-11 12:37:56 -0500

    Added playing function

--- commands/music/join_channel.js
+++ commands/music/join_channel.js
@@ -1,4 +1,18 @@
 const commando = require("discord.js-commando");
+const YTDL = require("ytdl-core");
+
+function play(connection, message) {
+  var server = servers[message.guild.id];
+  server.dispatcher = connection.playStream(YTDL(server.queue[0, {filter: "audioonly"}]));
+  server.queue.shift();
+  server.dispatcher.on("end", function() {
+    if(server.queue[0]) {
+      play(connection, message);
+    } else {
+      connection.disconnect();
+    }
+  });
+}
 
 class JoinChannelCommand extends commando.Command {
   constructor (client) {
@@ -11,13 +25,18 @@ class JoinChannelCommand extends commando.Command {
   }
 
   async run(message, args) {
-    if(message.member.voiceChannel) {
-      if(!message.guild.voiceConnection) {
-        message.member.voiceChannel.join()
-          .then(connection => {
-            message.reply("Successfully joined!");
-          });
+    if(message.member.voiceChannel && !message.guild.voiceConnection) {
+      if(!servers[message.guild.id]) {
+        servers[message.guild.id] = {queue: []};
       }
+      message.member.voiceChannel.join()
+        .then(connection => {
+          var server = servers[message.guild.id];
+          message.reply("Successfully joined!");
+          server.queue.push(args);
+          play(connection, message);
+        });
+
     } else {
       message.reply("You must be in a voice channel first.");
     }
